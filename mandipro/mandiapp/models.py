ce4e77fbe7c7b3e5f11c7f274b0e98fafb993456
arpit <anigam075@gmail.com>
2024-07-25 22:44:53 +0530

    defining model

--- mandipro/mandiapp/models.py
+++ mandipro/mandiapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Sabjimandi(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.CharField(max_length=2000, null=True, blank=True)
+    created_date = models.CharField(max_length=255, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
